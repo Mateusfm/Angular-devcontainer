679f48035c6b6ec2cf7e0796ca65c382b768caab
Mateus Martins <mateufm95@gmail.com>
2026-06-21 17:56:17 +0000

    feat: Adicionando menu lateral

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [RouterOutlet],
+	imports: [RouterOutlet, MatButtonModule, MatListModule, MatSidenavModule, MatToolbarModule],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.css',
 })
 export class AppComponent {
-	title = 'angular18';
+	readonly title = 'angular18';
+
+	readonly menuItems = [
+		{ label: 'Início' },
+		{ label: 'Projetos' },
+		{ label: 'Relatórios' },
+		{ label: 'Configurações' },
+	];
 }
